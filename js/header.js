feb5681c546c501c03ec9a2cf85cb01c206d2748
Jefferson Teng <jteng3@gmail.com>
2016-01-12 23:40:27 -0800

    Move header and footer into JS scripts
    http://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file

---
+++ js/header.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jteng on 1/12/16.
+ */
+/**
+ * Since this site has multiple pages that share a header, but has no server
+ * side code, we use this script to inject the header into the HTML.
+ */
+(function() {
+    document.write('\
+        <header class="header">\
+        <span class="name">Jefferson Teng</span>\
+        <span class="nav-btn-container">\
+        <span class="nav-btn">Hello</span>\
+        <span class="nav-btn">Hello</span>\
+        <span class="nav-btn">Hello</span>\
+        </span>\
+        </header>\
+    ');
+})();
